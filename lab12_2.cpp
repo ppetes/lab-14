90df3a3e103e2ef05dcc927af9c2e3d298488b4c
neokarn <34811101+neokarn@users.noreply.github.com>
2018-02-18 21:54:53 +0700

    Create lab12_2.cpp

---
+++ lab12_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 30;
+const int M = 70;
+
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
+
+int main()
+{
+    bool image[N][M] = {};
+    int s,x,y;
+    do{
+        showImage(image);
+        cout << "Input your brush size and location:";
+        cin >> s >> x >> y;
+        updateImage(image,s,x,y);
+    }while(s != 0 && x != 0 && y != 0 );
+             return 0;    
+}
+
+// Write definition of updateImage() and showImage() here
