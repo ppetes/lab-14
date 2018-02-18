9952e605503be2338de09644cb902de9d432e506
neokarn <34811101+neokarn@users.noreply.github.com>
2018-02-18 21:46:39 +0700

    Create lab12_1.cpp

---
+++ lab12_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void stat(const double[],int,double []);
+
+int main()
+{
+    double A[] = {1.2,-3.5,6.9,7.8,12.5,-0.5};
+    int N = sizeof(A)/sizeof(A[0]);
+    double B[4];
+    stat(A,N,B);
+    cout << "Average = " << B[0];
+    cout << "\nS.D. = " << B[1];
+    cout << "\nMax = " << B[2];
+    cout << "\nMin = " << B[3];
+    return 0;
+}
+
+//Write definition of stat() here 
