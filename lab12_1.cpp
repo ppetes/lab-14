485a6693fb31d3ad72a303ad9713ebfa75a70523
neokarn <34811101+neokarn@users.noreply.github.com>
2021-01-16 15:34:37 +0700

    Update lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -2,16 +2,18 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double []);
+void stat(const double[],int,double[]);
 
 int main(){
-    double A[] = {1.2,-3.5,6.9,7.8,12.5,-0.5};
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
     int N = sizeof(A)/sizeof(A[0]);
-    double B[4];
+    double B[6];
     stat(A,N,B);
-    cout << "Average = " << B[0];
-    cout << "\nS.D. = " << B[1];
-    cout << "\nMax = " << B[2];
-    cout << "\nMin = " << B[3];
+    cout << "Arithmetic Mean = " << B[0];
+    cout << "\nStandard Deviation = " << B[1];
+    cout << "\nGeometric Mean = " << B[2];
+    cout << "\nHarmonic Mean = " << B[3];
+    cout << "\nMax = " << B[4];
+    cout << "\nMin = " << B[5];
     return 0;
 }
