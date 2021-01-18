78d5e3cf93869b987ec87784ebab2f90192118c9
neokarn <34811101+neokarn@users.noreply.github.com>
2021-01-18 13:57:59 +0700

    Update lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -15,7 +15,7 @@ int main()
     int s,x,y;
     do{
         showImage(image);
-        cout << "Input your brush size and location:";
+        cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
     }while(s != 0 && x != 0 && y != 0 );
