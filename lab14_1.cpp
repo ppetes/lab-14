b493ab166f9a0413bd73c3aeb9e3c6dc265c5103
ratchapon prangthong <ppete2547@gmail.com>
2023-01-13 16:35:57 +0700

    lab14_1 fish

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,61 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int n ,double B[]){
+double sum1 = 0;
+double sum2 = 0;
+double sum3 = 0;
+double sum4 = 1;
+double max = 0,min = 1;
+double gm;
+    for(int i = 0;i<n;i++){
+        sum1 += A[i];
+        B[0] = sum1/n;
+    }
+   for(int i = 0;i<n;i++){
+   double sd;
+        sum2 += pow(A[i],2);
+        sd = sqrt((sum2/n)-pow(B[0],2));
+        B[1] = sd;
+    }
+     for(int i = 0;i<n;i++){
+        
+       sum4 *= A[i];}
+       gm = pow(sum4,(double)1/n);
+        B[2] = gm;
+    
+    
+      for(int i = 0;i<n;i++){
+        double hn;
+       sum3 += 1/A[i];
+       hn = n/sum3;
+        B[3] = hn;
+    }
+     for(int i = 0;i<n;i++){
+        
+        if(A[i]>max) max=A[i];
+        if(A[i]<min) min=A[i];
+       B[4] = max;
+       B[5] = min;
+    }
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
