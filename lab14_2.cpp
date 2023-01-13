ea19bf9c406b45ce7e6aa595d0e5db0c59514010
ratchapon prangthong <ppete2547@gmail.com>
2023-01-13 16:36:15 +0700

    lab14_2 fish

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool aray[][M],int s,int x,int y){
+for(int i = 0;i < 30;i++){
+    for(int j = 0;j < 70;j++){
+    if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1){
+    aray[i][j] = 10;}
+    }
+    }
+    }
+    void showImage(const bool x[][M]){
+
+for(int i = 0;i < 72;i++){
+cout << "-";
+}
+cout << "\n";
+for(int i = 0;i < 30;i++){
+cout << "|";
+for(int j = 0;j < 70 ; j++){
+if(x[i][j] == 0) cout << " ";
+else cout << "*";
+}
+cout << "|\n";
+}
+for(int i = 0;i < 72;i++){
+cout << "-";
+}
+cout << "\n";
+}
